6eb8f7da05ce81a701c2e766acf77661d5857763
Nekuso77 <matthieu.picot83@gmail.com>
2025-01-26 23:21:52 +0100

    add CP reduction optional + update GridSearchCV for SVC + add implementation of best_parameters

--- CreditCardFraudDetection/mainCreditCardFraudDetection.py
+++ CreditCardFraudDetection/mainCreditCardFraudDetection.py
@@ -32,9 +32,12 @@ from sklearn.metrics import RocCurveDisplay
 ## FLAG
 ##############################################################################
 flag_undersampling = 1 # Flag pour activer ou non le sous-échantilonnage
-flag_randomforest = 0 # Flag pour activer ou non les Forêts Aléatoires
+flag_reductionACP = 0 # Flag pour activer ou non la reduction des CP
+
+flag_randomforest = 1 # Flag pour activer ou non les Forêts Aléatoires
 flag_linearSVC = 0 # Flag pour activer ou non la linearSVC
 flag_SVC = 1 # Flag pour activer ou non la SVC
+
 flag_research_rf = 0 # Flag pour activer ou non la recherche des meilleurs paramètres pour RandomForest
 flag_research_linearSVC = 0 # Flag pour activer ou non la recherche des meilleurs paramètres pour LinearSVC
 flag_research_SVC = 0 # Flag pour activer ou non la recherche des meilleurs paramètres pour SVC
@@ -49,20 +52,39 @@ data = pd.read_csv(filename, delimiter=',')
 # print(data)
 print(f"Dimensions du dataset : {data.shape}")
 
-# # Optionel
-# import seaborn as sns
+# Optionel
+import seaborn as sns
 
-# sns.countplot(data=data, x="Class", hue="Class", stat="percent")
-# plt.title("Nombre de transactions concernés par la fraude")
-# plt.show()
+sns.countplot(data=data, x="Class", hue="Class", stat="percent")
+plt.title("Nombre de transactions concernées par la fraude")
+plt.show()
 
 ## Traitement des données ##
 
 random_state = 77 # On fixe un random_state pour la répétabilité
 
-# Séparation descripteurs/étiquettes
-X = data.drop(columns=["Class"]) # Descripteurs
-y = data["Class"] # Etiquettes
+### Reduction de dimensions des CP ###
+if flag_reductionACP == 1 :
+    # Selection des colonnes de V1 à V28
+    acp_columns = [f"V{i}" for i in range(1, 29)]
+    
+    # Calcul de la corrélation de chaque CP avec 'class' en supprimant la dernière ligne correspondant à la correlation de 'class' avec 'class'
+    correlations = data[acp_columns + ['Class']].corr()['Class'].iloc[:-1]
+    
+    # Trier le vecteur des corrélations dans l'ordre décroissant
+    correlations_sorted = correlations.sort_values(ascending=False)
+    
+    # Recupération des meilleurs 10 CP en fonction de la corrélation positive et négative 
+    best_correlations_columns = correlations_sorted.head(10).index.tolist() + correlations_sorted.tail(10).index.tolist()
+
+    # Séparation descripteurs/étiquettes
+    columns_list = best_correlations_columns + ["Time", "Amount"]
+    X = data[columns_list] # Descripteurs
+    y = data["Class"] # Etiquettes
+else :
+    # Séparation descripteurs/étiquettes
+    X = data.drop(columns=["Class"]) # Descripteurs
+    y = data["Class"] # Etiquettes
 
 # Séparation du dataset en données d'entrainement et de test
 train_size = 0.7
@@ -79,7 +101,7 @@ if flag_undersampling == 1 :
     rus = RandomUnderSampler(random_state=random_state)
     X_train, y_train = rus.fit_resample(X_train, y_train)
     print("Sous échantillonnage")
-    print("Dimensions de la base d'entrainement : {X_train.shape}")
+    print(f"Dimensions de la base d'entrainement : {X_train.shape}")
     print("Répartition des données :")
     print(y_train.value_counts())
 
@@ -101,6 +123,13 @@ if flag_research_rf == 1 :
         #  'max_depth': 9,
         #  'min_samples_leaf': 1,
         #  'min_samples_split': 4,
+        #  'n_estimators': 100}
+        
+    # Résultat 26/01/2025 (Avec Reduction CP):
+        # {'criterion': 'gini',
+        #  'max_depth': 11,
+        #  'min_samples_leaf': 1,
+        #  'min_samples_split': 3,
         #  'n_estimators': 100}
         
     # Rechercher des meilleurs paramètres avec la méthode GridSearchCV
@@ -134,39 +163,44 @@ if flag_research_linearSVC == 1:
 
 ## Recherche pour SVC
 if flag_research_SVC == 1:
-    ### ESSAI AVEC GRIDSEARCHCV NON CONCLUANT ###
-    # # Ensemble de paramètres à tester pour la SVC
-    # params_SVC = {
-    #     "C" : list(range(1,20)),
-    #     "kernel" : ["poly", "rbf", "sigmoid"],
-    #     #"degree" : list(range(1,6)),
-    #     "gamma" : list(range(1,10))
-    #     }
-  
-    # # Rechercher les meilleurs paramètres avec la méthode GridSearchCV
-    # clf_SVC = SVC(random_state=random_state)
-    # clf_SVC_cv = GridSearchCV(clf_SVC, params_SVC, scoring="accuracy", n_jobs=-1, verbose=1, cv=3)
-    # clf_SVC_cv.fit(X_train, y_train)
+    ### ESSAI AVEC GRIDSEARCHCV ###
+    # Ensemble de paramètres à tester pour la SVC
+    params_SVC = {
+        "C" : list(range(1,100)),
+        "kernel" : ["rbf", "sigmoid"],
+        "gamma" : list(range(1,25))
+        }
     
-    # print("(SVC) Meilleurs paramètres trouvés :")
-    # print(clf_SVC_cv.best_params_)
+    # Résultat 26/01/2025
+        # {'C': 1,
+        #  'gamma': 1,
+        #  'kernel': 'rbf'}
+  
+    # Rechercher les meilleurs paramètres avec la méthode GridSearchCV
+    clf_SVC = SVC(random_state=random_state)
+    clf_SVC_cv = GridSearchCV(clf_SVC, params_SVC, scoring="accuracy", n_jobs=-1, verbose=1)
+    clf_SVC_cv.fit(X_train, y_train)
+    
+    print("(SVC) Meilleurs paramètres trouvés :")
+    print(clf_SVC_cv.best_params_)
+    print(f"Score : {clf_SVC_cv.best_score_}")
     
     ### ESSAI AVEC LA TECHNIQUE SIMPLE VU EN COURS ###
-    # Recherche des meilleurs hyperparamètres
-    c_test = [0.01,0.03,0.1,0.3,1,3,10,100]
-    g_test = [0.05,0.05,0.5,5,10,15,20,25]
-    best_score = 0
-    for c in c_test:
-        for g in g_test:
-            clf_SVC=SVC(C=c, gamma=g, kernel="rbf") 
-            clf_SVC.fit(X_train,y_train)
-            print(f"C = {c} | gamma= {g} -> Score : {clf_SVC.score(X_train,y_train)}")
-            if clf_SVC.score(X_train,y_train) > best_score:
-                best_score = clf_SVC.score(X_train,y_train)
-                best_c = c
-                best_g = g
+    # # Recherche des meilleurs hyperparamètres
+    # c_test = [0.01,0.03,0.1,0.3,1,3,10,100]
+    # g_test = [0.05,0.05,0.5,5,10,15,20,25]
+    # best_score = 0
+    # for c in c_test:
+    #     for g in g_test:
+    #         clf_SVC=SVC(C=c, gamma=g, kernel="rbf") 
+    #         clf_SVC.fit(X_train,y_train)
+    #         print(f"C = {c} | gamma= {g} -> Score : {clf_SVC.score(X_train,y_train)}")
+    #         if clf_SVC.score(X_train,y_train) > best_score:
+    #             best_score = clf_SVC.score(X_train,y_train)
+    #             best_c = c
+    #             best_g = g
     
-    print(f" (SVC) Les meilleurs hyperparamètres sont : C = {best_c} et gamma = {best_g} avec un score de {best_score}")
+    # print(f" (SVC) Les meilleurs hyperparamètres sont : C = {best_c} et gamma = {best_g} avec un score de {best_score}")
     
 ##############################################################################
 ## CLASSIFICATION AVEC FORETS ALEATOIRES
@@ -247,16 +281,18 @@ if flag_SVC == 1 :
     # Par défaut #
     # clf_SVC = SVC(random_state=random_state) # Création d'un Classifieur SVC
     
-    # Avec les meilleurs paramètres trouvés avec Technique vu en cours #
+    # Avec les meilleurs paramètres trouvés #
     # Liste des meilleurs paramètres :
     best_c = 1
-    best_g = 0.05
+    best_g = 1
+    best_kernel = "rbf"
     
     # Création du modèle
     clf_SVC = SVC(
         random_state=random_state,
         C=best_c,
-        gamma=best_g)
+        gamma=best_g,
+        kernel=best_kernel)
     
     clf_SVC.fit(X_train, y_train) # Entrainement du modèle
     
